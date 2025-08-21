1be67b25f29ffd46fbfca8ca0d685ce2696f4248
Migo Ildesa <migo.ildesa@gmail.com>
2025-08-21 15:30:06 +0900

    added BookController, Book model and home view modified migration and seeder

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Models\Book;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
-    return view('welcome');
+    $books = Book::all();
+    if (session('success')) {
+        return view('home', [
+            'books' => $books,
+            'success' => session('success'),
+        ]);
+    }
+    return view('home', compact('books'));
 });
+
+Route::post('/borrow', [BookController::class, 'borrow']);
+Route::post('/return', [BookController::class, 'return']);
