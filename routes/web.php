f5df9b9bee16c9294f1893ede19bca84c05a5ad4
anXan0711 <arakakishota@icloud.com>
2025-09-11 15:02:08 +0900

    feat: 初回アップロード

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
+// ルートパスで borrowing_records.index を表示
 Route::get('/', function () {
-    return view('test');
+    $books = DB::table('books')
+        ->whereNotNull('borrower')
+        ->where('borrower', '!=', '')
+        ->where('borrower', '!=', '借りていない')
+        ->select('book_title', 'borrower')
+        ->orderBy('book_title')
+        ->get();
+
+    return view('borrowing_records.index', ['books' => $books]);
+});
+
+Route::get('/borrowing-records', function () {
+    $books = DB::table('books')
+        ->whereNotNull('borrower')
+        ->where('borrower', '!=', '')
+        ->where('borrower', '!=', '借りていない')
+        ->select('book_title', 'borrower')
+        ->orderBy('book_title')
+        ->get();
+
+    return view('borrowing_records.index', ['books' => $books]);
 });
