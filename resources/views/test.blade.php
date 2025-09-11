c3d9833083529c3c457ab89818a58d88a3ab7b84
kenken <anken914@ezweb.ne.jp>
2025-09-11 16:28:22 +0900

    共通ヘッダーの作成、test.blade.phpに読みこみ

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,18 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ボタン配色テスト</title>
-    <!-- Tailwind を読み込む -->
-    <!-- @theme を書いた CSS を読み込む -->
-    @vite('resources/css/app.css')
-</head>
+@include('layouts.header')
 <body>
-
-    <button class="bg-primary text-white font-sans rounded-[var(--btn-radius)] px-[var(--btn-padding-x)] py-[var(--btn-padding-y)]">
-        メインボタン
-    </button>
 
     <p class="text-textmain font-sans text-title">
         これはテストページです。
@@ -21,6 +10,10 @@
     <p class="text-accent font-sans text-body">
         これはサブテキストです。
     </p>
+
+    <button class="bg-primary text-white font-sans rounded-[var(--btn-radius)] px-[var(--btn-padding-x)] py-[var(--btn-padding-y)]">
+        メインボタン
+    </button>
 
 </body>
 </html>
