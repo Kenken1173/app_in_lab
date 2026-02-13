f451b1a1dc8c4dbafdcb87957749c18f38bbbd24
mac_ken <anken914_3@ezweb.ne.jp>
2026-02-13 14:56:51 +0900

    open library apiを使用して、apiの実装を完了させた。 日本語版と英語版の書籍それぞれ返せるように二つ関数を用意した。
    ただし、検索自体は英語で行わないと、検索結果が極端に少なくなる。

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TestController;
+use App\Http\Controllers\ApiController;
 
-Route::get('/testapi/{name}', [TestController::class, 'search_book']);
+Route::get('/testapi/en/{name}', [ApiController::class, 'search_book_for_en']);
+Route::get('/testapi/ja/{name}', [ApiController::class, 'search_book_for_ja']);
